fc42b3e650609afeb2fa656cacb8533a82e20e39
luhoctap2409 <134180810+lunt240923@users.noreply.github.com>
2023-08-03 16:47:16 +0700

    first commit

---
+++ src/main/java/com/cphone/WebsiteCellPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.cphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteCellPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteCellPhoneApplication.class, args);
+	}
+
+}
